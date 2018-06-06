b8e7a41410a26e4c7731883b1a02cc32be5a0784
zhengzheming <7upcat@gmail.com>
2018-06-06 20:34:42 +0800

    优化了初始化脚本

--- database/init.sql
+++ database/init.sql
@@ -1,21 +1,20 @@
 -- 参数
-INSERT INTO CONFIG VALUES('reap.prd.default.server.logging.path','reap','prd','default','logging.path','/tmp');
-INSERT INTO CONFIG VALUES('reap.prd.default.server.compression.enable','reap','prd','default','server.compression.enable','true');
-INSERT INTO CONFIG VALUES('reap.prd.default.server.compression.enable','reap','prd','default','server.compression.enable','true');
-INSERT INTO CONFIG VALUES('reap.prd.default.server.compression.mime-types','reap','prd','default','server.compression.mime-types','pplication/json,application/xml,text/html,text/xml,text/plain,application/javascript,text/css');
-INSERT INTO CONFIG VALUES('reap.prd.default.spring.resources.static-locations', 'reap', 'prd', 'default','spring.resources.static-locations','file:apps/${spring.application.name}/static,classpath:static');
-INSERT INTO CONFIG VALUES('reap.prd.default.spring.datasource.url', 'reap', 'prd', 'default', 'spring.datasource.url', 'jdbc:mysql://119.29.92.158/reapdb');
-INSERT INTO CONFIG VALUES('reap.prd.default.spring.datasource.username', 'reap', 'prd', 'default', 'spring.datasource.username', 'reap');
-INSERT INTO CONFIG VALUES('reap.prd.default.spring.datasource.password', 'reap', 'prd', 'default', 'spring.datasource.password', 'reap');
-INSERT INTO CONFIG VALUES('reap.prd.default.spring.datasource.driver-class-name', 'reap', 'prd', 'default', 'spring.datasource.driver-class-name', 'com.mysql.jdbc.Driver');
-INSERT INTO CONFIG VALUES('reap.prd.default.spring.jpa.open-in-view', 'reap', 'prd', 'default', 'spring.jpa.open-in-view', 'true');
-INSERT INTO CONFIG VALUES('reap.prd.default.spring.jpa.show-sql', 'reap', 'prd', 'default', 'spring.jpa.show-sql', 'true');
-INSERT INTO CONFIG VALUES('reap.prd.default.spring.jpa.properties.hibernate.format_sql', 'reap', 'prd', 'default', 'spring.jpa.properties.hibernate.format_sql', 'true');
-INSERT INTO CONFIG VALUES('reap.prd.default.spring.messages.basename', 'reap', 'prd', 'default', 'spring.messages.basename', 'i18n/messages');
-INSERT INTO CONFIG VALUES('reap.prd.default.eureka.client.initial-instance-info-replication-interval-seconds','reap','prd','default','eureka.client.initial-instance-info-replication-interval-seconds','10');
-INSERT INTO CONFIG VALUES('reap.prd.default.eureka.client.instance-info-replication-interval-seconds','reap','prd','default','eureka.client.instance-info-replication-interval-seconds','10');
+INSERT INTO CONFIG VALUES('r.p.d.s.l.path','reap','prd','default','logging.path','/tmp');
+INSERT INTO CONFIG VALUES('r.p.d.s.c.enable','reap','prd','default','server.compression.enable','true');
+INSERT INTO CONFIG VALUES('r.p.d.s.c.mime-types','reap','prd','default','server.compression.mime-types','pplication/json,application/xml,text/html,text/xml,text/plain,application/javascript,text/css');
+INSERT INTO CONFIG VALUES('r.p.d.s.r.static-locations', 'reap', 'prd', 'default','spring.resources.static-locations','file:apps/${spring.application.name}/static,classpath:static');
+INSERT INTO CONFIG VALUES('r.p.d.s.d.url', 'reap', 'prd', 'default', 'spring.datasource.url', 'jdbc:mysql://119.29.92.158/reapdb');
+INSERT INTO CONFIG VALUES('r.p.d.s.d.username', 'reap', 'prd', 'default', 'spring.datasource.username', 'reap');
+INSERT INTO CONFIG VALUES('r.p.d.s.d.password', 'reap', 'prd', 'default', 'spring.datasource.password', 'reap');
+INSERT INTO CONFIG VALUES('r.p.d.s.d.driver-class-name', 'reap', 'prd', 'default', 'spring.datasource.driver-class-name', 'com.mysql.jdbc.Driver');
+INSERT INTO CONFIG VALUES('r.p.d.s.j.open-in-view', 'reap', 'prd', 'default', 'spring.jpa.open-in-view', 'true');
+INSERT INTO CONFIG VALUES('r.p.d.s.j.show-sql', 'reap', 'prd', 'default', 'spring.jpa.show-sql', 'true');
+INSERT INTO CONFIG VALUES('r.p.d.s.j.p.h.format_sql', 'reap', 'prd', 'default', 'spring.jpa.properties.hibernate.format_sql', 'true');
+INSERT INTO CONFIG VALUES('r.p.d.s.m.basename', 'reap', 'prd', 'default', 'spring.messages.basename', 'i18n/messages');
+INSERT INTO CONFIG VALUES('r.p.d.e.c.i-i-i-r-i-s','reap','prd','default','eureka.client.initial-instance-info-replication-interval-seconds','10');
+INSERT INTO CONFIG VALUES('r.p.d.e.c.i-i-r-i-s','reap','prd','default','eureka.client.instance-info-replication-interval-seconds','10');
 
-INSERT INTO CONFIG VALUES('reap-facility.prd.default.server.port','reap-facility','prd','default','server.port','8761');
+INSERT INTO CONFIG VALUES('r-f.p.d.s.p','reap-facility','prd','default','server.port','8761');
 
 -- 路由规则
 INSERT INTO ROUTE VALUES('reap','reap','/reap/**','reap-portal',null,null); -- 统一 UI路由
