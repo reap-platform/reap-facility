b9d95d8a85e750d2d8519e7c235005accb339873
zhengzheming <7upcat@gmail.com>
2018-06-07 18:09:32 +0800

    修复了集成至 reap portal 无法正常展示的 BUG

--- src/web/src/app/index.js
+++ src/web/src/app/index.js
@@ -4,7 +4,6 @@ import { EVENT_TYPE_LIFECYCLE_READY, EVENT_TYPE_UI_RESET_VIEW, EVENT_TYPE_LIFECY
  * 应用启动的初始化函数，用于同 GUIP 窗口间传递应用初始化所需要使用的信息（如用户、token 等）
  */
 export function init (cb) {
-  // 对于
   if (window.parent !== window) {
     window.addEventListener('message', (event) => {
       if (event.data.type === EVENT_TYPE_LIFECYCLE_START) {
--- src/web/src/constants/index.js
+++ src/web/src/constants/index.js
@@ -1,3 +1,11 @@
+/** 事件类型：重置 UI. */
+export const EVENT_TYPE_UI_RESET_VIEW = 'resetView'
+
+/** 事件类型：应用准备就绪. */
+export const EVENT_TYPE_LIFECYCLE_READY = 'ready'
+
+/** 事件类型：应用启动. */
+export const EVENT_TYPE_LIFECYCLE_START = 'start'
 
 /** 默认的分页大小. */
 export const DEFAULT_PAGE_SIZE = 10
