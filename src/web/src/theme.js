20d60778b2c9c0be7b7df3a3ed089f6b31d810ae
zhangy1103 <zhangy1103@zhangy1103deiMac.local>
2018-06-20 17:06:39 +0800

    修改字体文件

--- src/web/src/theme.js
+++ src/web/src/theme.js
@@ -1,4 +1,4 @@
 // https://github.com/ant-design/ant-design/blob/master/components/style/themes/default.less
 module.exports = {
-  '@icon-url': "'../../../../../src/assets/iconfont/font_123'",
+  '@icon-url': "'../../../../../src/assets/iconfont/font'",
 }
